ba9d200d3820a3e1db5fe0ca2e5df1b0607d2f11
practica <velazquezhernandezjesus13@gmail.com>
2023-01-22 12:40:45 -0600

    cuarto commit

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,11 +1,14 @@
 import {BrowserRouter, Routes,Route} from "react-router-dom";
 import HojaVida from "../Pages/HojaVida";
+import NotFound from "../Pages/NotFound";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HojaVida/>} />
+                <Route path="/Jesus-137/HojaVida.git/Jesus-137" element={<HojaVida/>} />
+                <Route path="/HojaVida/" element={<HojaVida/>}/>
+                <Route path="/*" element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
     );
